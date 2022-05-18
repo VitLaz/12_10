6b663c8070566a5b09896cebd04322d493efbe01
Lazarev Vitaly <Curcul84@gmail.com>
2022-05-18 16:45:24 +0300

    sa

--- src/test/java/curcul/page/RegistrationFromPage.java
+++ src/test/java/curcul/page/RegistrationFromPage.java
@@ -39,73 +39,88 @@ public class RegistrationFromPage {
         return this;
     }
 
+    @Step("Вводим имя")
     public RegistrationFromPage setFirstName (String firstName) {
         firstNameLoc.setValue(firstName);
         return this;
     }
 
+    @Step("Вводим фамилию")
     public RegistrationFromPage setLastName (String lastName){
         lastNameLoc.setValue(lastName);
         return this;
     }
 
+    @Step("Вводим почту")
     public RegistrationFromPage setEmail (String email) {
         emailLoc.setValue(email);
         return this;
     }
 
+    @Step("Вводим пол")
     public RegistrationFromPage setGender (String gender) {
         genderLoc.$(byText(gender)).click();
         return this;
     }
 
+    @Step("Вводим телефон")
     public RegistrationFromPage setTele (String teleNumber) {
         teleNumberLoc.setValue(teleNumber);
         return this;
     }
 
+    @Step("Вводим день рождение")
     public RegistrationFromPage setBirthDate (String day, String month, String year) {
         birthDateLoc.click();
         calendar.setDate(day, month, year);
         return this;
     }
 
+    @Step("Вводим предметы")
     public RegistrationFromPage setSubjects (String subject) {
         subjectLoc.setValue(subject).pressEnter();
         return this;
     }
 
+    @Step("Вводим хобби")
     public RegistrationFromPage setHobbies (String hobbies) {
         hobbiesLoc.$(byText(hobbies)).click();
         return this;
     }
 
+    @Step("Вставляем файл")
     public RegistrationFromPage setUploadFile (String file) {
         pictureLoc.uploadFromClasspath(file);
         return this;
     }
 
+    @Step("Вводим адрес")
     public RegistrationFromPage setCurrentAddress (String currentAddress) {
         addressLoc.setValue(currentAddress);
         return this;
     }
 
+    @Step("Выбираем штат")
     public RegistrationFromPage setState (String state) {
         stateLoc.click();
         $(byText(state)).click();
         return this;
     }
 
+    @Step("Выбираем город")
     public RegistrationFromPage setCity (String city) {
         cityLoc.click();
         $(byText(city)).click();
         return this;
     }
 
+    @Step("Нажимаем на кнопку")
     public RegistrationFromPage setSubmit (){
         submitLoc.click();
         return this;
     }
+
+    @Step("Проверяем отображение введенных данных")
     public RegistrationFromPage checkForm (String fieldName, String value) {
         resultsTableLoc.$(byText(fieldName)).parent().shouldHave(text(value));
         return this;
